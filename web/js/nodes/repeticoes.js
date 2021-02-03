64cb3f9c14d4c3224a4fecd0f2a96534af8b77d2
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-03 09:59:25 -0300

    brabando

---
+++ web/js/nodes/repeticoes.js
@@ -0,0 +1,14 @@
+const locaisViajem = new Array
+(
+    'Filipinas',
+    'Tailandia',
+    'Polonesia Francesa',
+    'Maranhão'
+)
+
+let cont = 0
+while (cont<4)
+{
+    console.log(locaisViajem[cont]);
+    cont +=1
+}
