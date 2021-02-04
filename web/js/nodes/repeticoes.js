ebd0fa2bba4a7234d62abcdce3b7ba0e61051822
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-04 18:39:40 -0300

    night

--- web/js/nodes/repeticoes.js
+++ web/js/nodes/repeticoes.js
@@ -1,14 +1,16 @@
-const locaisViajem = new Array
-(
-    'Filipinas',
-    'Tailandia',
-    'Polonesia Francesa',
-    'Maranhão'
-)
+var produto = document.querySelector(".produto1")
 
-let cont = 0
-while (cont<4)
-{
-    console.log(locaisViajem[cont]);
-    cont +=1
-}
+var estoque = produto.querySelector('.info-estoque')
+var estq = estoque.textContent
+var vUnitario = produto.querySelector('.info-unidade')
+var unid = vUnitario.textContent
+
+var vTotal = produto.querySelector(".info-total")
+var total = vTotal.innerHTML
+console.log(total)
+
+total = estq * unid
+
+console.log(estq)
+console.log(unid)
+console.log(total)
