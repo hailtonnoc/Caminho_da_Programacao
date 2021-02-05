d174c416809d84ca3b8869a6aba5559c0895eda1
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-05 19:26:04 -0300

    night

--- web/js/nodes/repeticoes.js
+++ web/js/nodes/repeticoes.js
@@ -37,11 +37,36 @@ for(var i = 0; i < produtos.length ; i++)
     }
 }
 
-
-const botao = document.getElementById('salvar')
-
-botao.addEventListener('click', function()
+var botao = document.querySelector('#salvar')
+botao.addEventListener('click', function(event)
 {
-    alert('funcionando catchorro')
+    event.preventDefault()
+
+//pegar elementos
+var form = document.querySelector('.entrada')
+
+var produto = form.produto.value;
+var quantidade = form.quantidade.value;
+var unidade = form.vunidade.value;
+
+//criar elementos
+var produtoTr = document.createElement("tr")
+
+var produtoTd = document.createElement("td")
+var quantidadeTd = document.createElement("td")
+var unidadeTd = document.createElement("td")
+var totalTd = document.createElement("td")
+
+produtoTd.textContent = produto
+quantidadeTd.textContent = quantidade
+unidadeTd.textContent = unidade
+
+produtoTr.appendChild(produtoTd)
+produtoTr.appendChild(quantidadeTd)
+produtoTr.appendChild(unidadeTd)
+produtoTr.appendChild(totalTd)
+
+console.log(produtoTr)
+
 }
 )
