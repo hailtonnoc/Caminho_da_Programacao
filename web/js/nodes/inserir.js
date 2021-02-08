1231ccd18e2da5540b9b75834d836c60638282ce
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-07 21:20:45 -0300

    separando codigos

--- web/js/nodes/inserir.js
+++ web/js/nodes/inserir.js
@@ -1,42 +1,3 @@
-//selecionando elemento geral
-var produtos = document.querySelectorAll(".produto")
-
-for(var i = 0; i < produtos.length ; i++)
-{
-    var produto = produtos[i]
-    
-    //selecionando sub-elementos
-    var estoque = produto.querySelector('.info-estoque')
-    var estq = estoque.textContent
-    var vUnitario = produto.querySelector('.info-unidade')
-    var unid = vUnitario.textContent
-
-    var vTotal = produto.querySelector(".info-total")
-
-
-    //função
-    var temEstoque = true
-    var temValorUni = true
-
-    if(estq <= 0)
-    {
-        temEstoque = false 
-        vTotal.textContent = "Estoque invalido"
-    }
-
-    if(unid <= 0)
-    {
-        temValorUni = false
-        vTotal.textContent = "Valor unitário invalido"
-    }
-
-    if(temEstoque && temValorUni)
-    {
-        var total = estq * unid
-        vTotal.textContent = total
-    }
-}
-
 var botao = document.querySelector('#salvar')
 botao.addEventListener('click', function(event)
 {
@@ -60,6 +21,7 @@ var totalTd = document.createElement("td")
 produtoTd.textContent = produto
 quantidadeTd.textContent = quantidade
 unidadeTd.textContent = unidade
+totalTd.textContent = calculoTotal(quantidade, unidade)
 
 produtoTr.appendChild(produtoTd)
 produtoTr.appendChild(quantidadeTd)
---
+++ web/js/nodes/repetir.js
@@ -0,0 +1,46 @@
+//selecionando elemento geral
+var produtos = document.querySelectorAll(".produto")
+
+for(var i = 0; i < produtos.length ; i++)
+{
+    var produto = produtos[i]
+    
+    //selecionando sub-elementos
+    var estoque = produto.querySelector('.info-estoque')
+    var estq = estoque.textContent
+    var vUnitario = produto.querySelector('.info-unidade')
+    var unid = vUnitario.textContent
+
+    var vTotal = produto.querySelector(".info-total")
+
+
+    //função
+    var temEstoque = true
+    var temValorUni = true
+
+    if(estq <= 0)
+    {
+        temEstoque = false 
+        vTotal.textContent = "Estoque invalido"
+    }
+
+    if(unid <= 0)
+    {
+        temValorUni = false
+        vTotal.textContent = "Valor unitário invalido"
+    }
+
+    if(temEstoque && temValorUni)
+    {
+        var total = calculoTotal(estq, unid)
+        vTotal.textContent = total
+    }
+}
+
+function calculoTotal(quantidade, unidade)
+{
+    var total = 0
+
+    total = (quantidade * unidade)
+    return total
+}
