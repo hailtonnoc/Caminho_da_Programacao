737d16853450e49abb2d82167cebac3d1553d1fc
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-08 19:13:05 -0300

    nightmare

---
+++ web/js/nodes/filtre.js
@@ -0,0 +1,32 @@
+var campoFiltro = document.querySelector('#filtre')
+
+campoFiltro.addEventListener('input', function()
+{
+    console.log(this.value)
+    var produtos = document.querySelectorAll('.produto')
+    if( this.value.length > 0)
+    {
+        for(var i = 0; i < produtos.length ; i++ )
+        {
+            var produto = produtos[i]
+            var tdNome = produto.querySelector('.info-nome')
+            var nome = tdNome.textContent
+            if(nome != this.value)
+                {produto.classList.add('invisivel')}
+            else
+                {produto.classList.remove('invisivel')}
+        }
+    }
+    else
+    {
+        for(var i = 0; i < produtos.length ; i++ )
+        {
+            var produto = produtos[i]
+            produto.classList.remove('invisivel')
+        }
+    }
+
+})
+
+/*
+*/
