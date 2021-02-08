0da6515ea0771908e03cc409a126d3f1cb5bd976
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-07 22:11:55 -0300

    fim=

--- web/js/nodes/inserir.js
+++ web/js/nodes/inserir.js
@@ -5,12 +5,17 @@ botao.addEventListener('click', function(event)
 
 //pegar valor dos elementos
 var form = document.querySelector('.entrada')
-//                 name do input
+/*
+extraindo informações
 var produto = form.produto.value;
 var quantidade = form.quantidade.value;
 var unidade = form.vunidade.value;
+*/
+var produto = obtemProdutoEstoque(form)
+console.log(produto)
 
-//criar elementos
+/*
+criar elementos
 var produtoTr = document.createElement("tr")
 
 var produtoTd = document.createElement("td")
@@ -27,9 +32,47 @@ produtoTr.appendChild(produtoTd)
 produtoTr.appendChild(quantidadeTd)
 produtoTr.appendChild(unidadeTd)
 produtoTr.appendChild(totalTd)
+*/
+var produtoTr = montarTr(produto)
 
+//adicionando na pagina
 var tabela = document.querySelector('.corpoTabela')
 tabela.appendChild(produtoTr)
 
 }
 )
+
+function obtemProdutoEstoque(form)
+{
+    var produto = 
+    {
+        nome: form.produto.value,
+        quantidade: form.quantidade.value,
+        valorUnitario: form.vunidade.value,
+        total: calculoTotal(form.quantidade.value, form.vunidade.value)
+    }
+
+    return produto //return importante
+}
+
+function montarTr(produto)
+{
+    var produtoTr = document.createElement("tr")
+
+    var produtoTd = document.createElement("td")
+    var quantidadeTd = document.createElement("td")
+    var unidadeTd = document.createElement("td")
+    var totalTd = document.createElement("td")
+
+    produtoTd.textContent = produto.nome
+    quantidadeTd.textContent = produto.quantidade
+    unidadeTd.textContent = produto.valorUnitario
+    totalTd.textContent = produto.total
+
+    produtoTr.appendChild(produtoTd)
+    produtoTr.appendChild(quantidadeTd)
+    produtoTr.appendChild(unidadeTd)
+    produtoTr.appendChild(totalTd)
+
+    return produtoTr //ta vendo
+}
