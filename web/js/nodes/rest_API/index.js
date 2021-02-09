a1e80165a9a668c817faf65977ab9c11fd92033f
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-09 15:14:43 -0300

    afteroonzzzzzZzZzZ

--- web/js/nodes/rest_API/index.js
+++ web/js/nodes/rest_API/index.js
@@ -1,3 +1,6 @@
 const express = require('express')
 
 const app = express()
+
+app.listen(3000, function(){console.log('servidor rodando na porta 3000')})
+app.get('/atendimento', function(req, res){res.send('servidor dedicado a cadastro de atendimentos!')})
