e2a55a287d099a4e808750277d999a536c37953b
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-09 19:53:57 -0300

    fix night

--- web/js/nodes/rest_API/controllers/atendimentos.js
+++ web/js/nodes/rest_API/controllers/atendimentos.js
@@ -4,4 +4,10 @@ module.exports = app =>
     {
         res.send('servidor dedicado a cadastro de atendimentos! e isso foi inputado utilizando um GET')
     })
+
+    app.post('/atendimento', (req, res) => 
+    {
+        console.log(req.body)
+        res.send('rota de atendimentos e realizando um post')
+    })
 }
