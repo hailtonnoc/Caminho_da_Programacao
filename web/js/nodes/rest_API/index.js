2af9a5673dbda2829785b4229169b743be35a47c
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-10 10:32:14 -0300

    FIX

--- web/js/nodes/rest_API/index.js
+++ web/js/nodes/rest_API/index.js
@@ -1,6 +1,6 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
-
+const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro => 
     {
@@ -9,6 +9,7 @@ conexao.connect(erro =>
         else
             console.log('conectado com sucesso')
 
+            Tabelas.init(conexao)
             const app = customExpress()
             
             app.listen(3000, function(){console.log('servidor rodando na porta 3000')})
