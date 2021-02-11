75752357273e1aedfaa90a654efb8dc5df7afa48
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-11 11:12:05 -0300

    thursday

--- web/js/nodes/rest_API/models/atendimentos.js
+++ web/js/nodes/rest_API/models/atendimentos.js
@@ -7,16 +7,43 @@ class Atendimento
     {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 6
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) =>
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'O nome do cliente deve ter mais de 6 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros= erros.length
+
+        if(existemErros)
+        {res.status(400).json(erros)}
+        else
         {
-            if(erro)
-            {res.status(400).json(erro)}
-            else
-            {res.status(201).json(resultados)}
-        })
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro)
+                {res.status(400).json(erro)}
+                else
+                {res.status(201).json(resultados)}
+            })
+        }
+
+
     }
 
 }
