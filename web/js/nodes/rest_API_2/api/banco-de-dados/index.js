87462e76abd8dd1b5c93d7044004954b19e9d956
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-17 21:05:39 -0300

    good night

---
+++ web/js/nodes/rest_API_2/api/banco-de-dados/index.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const instancia = new Sequelize(
+    'petshop',
+    'root',
+    '123456',
+    {
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    }
+)
+
+module.exports = instancia
--- web/js/nodes/rest_API_2/api/index.js
+++ web/js/nodes/rest_API_2/api/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.listen(3000, )
+app.use(bodyParser.json())
+
+app.listen(3000, () => console.log('servidor rodando na porta 3000'))
