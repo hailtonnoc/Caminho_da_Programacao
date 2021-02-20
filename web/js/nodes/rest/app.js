ec6dc74a0531f5aee6982493330cacb0dabd70f0
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-20 19:08:22 -0300

    saturday on de hit

--- web/js/nodes/rest/app.js
+++ web/js/nodes/rest/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express();
 
 const rotaProdutos = require('./routes/produtos')
-
 app.use('/produtos', rotaProdutos);
+
+const rotaPedidos = require('./routes/pedidos')
+app.use('/pedidos', rotaPedidos);
 
 app.use('/caminho',(req, res, next) => 
 {
--- web/js/nodes/rest/routes/pedidos.js
+++ web/js/nodes/rest/routes/pedidos.js
@@ -1,3 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/',(req, res, next) => 
+{
+    res.status(200).send({
+        mensagem: 'Usando o get na rota /pedidos'
+    })
+});
+
+router.post('/', (req, res, next) => 
+{
+    res.status(201).send({
+        mensagem: 'Usando o post na rota/pedidos'
+    })
+});
+
+
+router.delete('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'estou pronto para deletar esses pedido de pipoca'
+    })
+})
+
+router.get('/:id_pedido', (req, res, next) => 
+{
+    const id = req.params.id_pedido
+    res.status(200).send({
+        mensagem: 'voce colocou o id ' + id
+    })
+})
+
+
+module.exports = router
