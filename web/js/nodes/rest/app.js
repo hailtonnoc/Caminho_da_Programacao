e1a2519d745d093ec3bcbecbde22562697fc08a0
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-21 16:44:27 -0300

    easy like sunday aaa aafternoon I WANNA BE HIGH!

--- web/js/nodes/rest/app.js
+++ web/js/nodes/rest/app.js
@@ -1,12 +1,35 @@
+//chamando modulos
 const express = require('express')
 const app = express();
 const morgan = require('morgan')
-app.use(morgan('dev'))
+const bodyParser = require('body-parser')
 
+//definindo arquivos
 const rotaProdutos = require('./routes/produtos')
-app.use('/produtos', rotaProdutos);
-
 const rotaPedidos = require('./routes/pedidos')
+
+//usando modulos
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//tratando o CORS --segurança
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*') 
+    //onde * seria o http://servidorQueConsome.com.lugar
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, content-Type, Accept, Authorization'
+    )
+    //definindo hearder de entrada de dados
+    if (req.method == 'OPTIONS')
+    {res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+    return res.status(200).send({})}
+    next();
+})
+
+//rotas, arquivos
+app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
 
--- web/js/nodes/rest/routes/pedidos.js
+++ web/js/nodes/rest/routes/pedidos.js
@@ -10,8 +10,14 @@ router.get('/',(req, res, next) =>
 
 router.post('/', (req, res, next) => 
 {
+    const pedido = 
+    {
+        id_produto: req.body.id_produto,
+        quantidade: req.body.quantidade
+    }
     res.status(201).send({
-        mensagem: 'Usando o post na rota/pedidos'
+        mensagem: 'Usando o post na rota/pedidos',
+        pedidoCriado: pedido
     })
 });
 
