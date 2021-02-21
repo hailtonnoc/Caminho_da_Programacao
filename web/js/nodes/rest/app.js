d1f727ca95f1697613889bdc7ee9c4184bb84238
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-02-21 12:54:48 -0300

    sunday afternoon

--- web/js/nodes/rest/app.js
+++ web/js/nodes/rest/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express();
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
 const rotaProdutos = require('./routes/produtos')
 app.use('/produtos', rotaProdutos);
@@ -7,11 +9,31 @@ app.use('/produtos', rotaProdutos);
 const rotaPedidos = require('./routes/pedidos')
 app.use('/pedidos', rotaPedidos);
 
+
+
+//aprendendo rotas
 app.use('/caminho',(req, res, next) => 
 {
     res.status(200).send({
-        mensagem: 'OK, TUDO NA MESMA PORRA'
+        mensagem: 'OK, TUDO CERTO, CAMINHO É O CAMINHO E `RES` É A RESPOSTA ATRAVES DO .SEND'
     });
 });
+
+//caso rota não encontrada
+app.use((req, res, next) => {
+    const erro = new Error('não encontrado');
+    erro.status = 404;
+    next(erro);
+})
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro:
+        {mensagem:error.message}
+    })
+})
+
+
+
 
 module.exports = app;
