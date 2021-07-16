cce7a4318f58b684dc6dda4ba7575bbeb14b5362
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-07-16 15:55:38 -0300

    fix debugando js

--- poo/python/cursovideo_ex/PycharmProjects/Estudos/Debugada Violenta/DebugatiOne.py
+++ poo/python/cursovideo_ex/PycharmProjects/Estudos/Debugada Violenta/DebugatiOne.py
@@ -1,7 +1,5 @@
-import csv
-with open('covid.csv', 'r', encoding='utf-8') as arcovid:
-    leitor = csv.reader(arcovid)
-    next(leitor)
-    for linha in leitor:
-        if int(linha[2]) > 5:
-            print(linha)
+import requests
+url = 'https://api.exchangerate-api.com/v6/latest'
+req= requests.get(url)
+
+print(req.status_code)
